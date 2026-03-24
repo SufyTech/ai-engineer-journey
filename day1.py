bd654a8a6b6836d171cdd84ab060b02d7e41e341
Sufiyan Khan <suzkhan135@gmail.com>
2026-03-24 22:38:21 +0530

    Day 1 - Sufiyan's AI Engineer journe starts. 24 March.

---
+++ day1.py
@@ -0,0 +1,13 @@
+name = "Sufiyan Khan"
+goal = "AI Engineer"
+internship = "Ethara"
+day_to_job = 53
+ 
+print("My name is", name)
+print("My goal is to become an", goal)
+print("I am currently doing an internship at", internship)
+print("I am", day_to_job, "days away from my dream job!")
+
+for day in range(1,54):
+    print(f"Day {day}: 10000 percent towards my goal")
+    
